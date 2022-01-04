3f244aef99ad8645c463757a1ba7a4d30cc09afc
lamine <laminejuvee@gmail.com>
2022-01-04 22:27:29 +0100

    displaying a map using leaflet library

--- script.js
+++ script.js
@@ -13,9 +13,20 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function (position) {
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        const coords = [latitude,longitude];
         console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     }, function () {
         alert("could not get your location");
     });
